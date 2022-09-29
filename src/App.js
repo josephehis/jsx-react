544932fe5f7147820e60b9716aec13f75e6c75ca
josephehis <101218648+josephehis@users.noreply.github.com>
2022-09-29 18:20:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
 import React from 'react';
 
 
-// const returnJSX = ()=> 'hello world';
 
-
-// function App(){
-//     const name = ['Sharon', 'John'];
-//     const nameObject = {
-//         firstName: 'Veron',
-//         LastName: 'John',
-//     }
-
-//     return(
-//         <div>
-//             <div className = 'divTag' styel={{backgroundColor: 'pink', width: '20%'}}>
-                
-//                    {/* {nameArray} */}
-//                    {nameObject.firstName}{nameObject.lastName}
-//                    hello world
-                
-//             </div>
-//             <label htmlFor= 'myInput'>
-//                    <input id= 'myInput'/>
-//             </label>
-//         </div>
-//     )
-// }
-
-    
-//return <div>hello world
-
-    //</div>
-    
-    
-
-
-//class App extends React.Component{
-    //render(){
-        //return <div>hello world</div>
-    
-   // }
-//}
 
 import Comment from './Comment';
 import Button from './Button';
