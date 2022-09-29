c3b7317b02d50bfe92cf6b566c9f63437725b413
josephehis <101218648+josephehis@users.noreply.github.com>
2022-09-29 18:26:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,46 +7,7 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
-// const returnJSX = ()=> 'hello world';
 
-
-// function App(){
-//     const name = ['Sharon', 'John'];
-//     const nameObject = {
-//         firstName: 'Veron',
-//         LastName: 'John',
-//     }
-
-//     return(
-//         <div>
-//             <div className = 'divTag' styel={{backgroundColor: 'pink', width: '20%'}}>
-                
-//                    {/* {nameArray} */}
-//                    {nameObject.firstName}{nameObject.lastName}
-//                    hello world
-                
-//             </div>
-//             <label htmlFor= 'myInput'>
-//                    <input id= 'myInput'/>
-//             </label>
-//         </div>
-//     )
-// }
-
-    
-// //return <div>hello world
-
-//     //</div>
-    
-    
-
-
-// //class App extends React.Component{
-//     //render(){
-//         //return <div>hello world</div>
-    
-//    // }
-// //}
 
 root.render(<App/>);
 
