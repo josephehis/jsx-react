2c731c175987c18f9172bcf8a7f83e4a5d458300
josephehis <101218648+josephehis@users.noreply.github.com>
2022-09-29 18:22:05 +0100

    Update Comment.js

--- src/Comment.js
+++ src/Comment.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from './Button';
 
 const comment= (props)=>{
-    //console.log(props, 'props')
+    
     return(
         <div style={
             {marginBottom: '10px', width: '30%', display: 'Flex', alignItems: 'center', flexDirection: 'column', backgroundColor: 'pink', border: 'solid thin black'}
